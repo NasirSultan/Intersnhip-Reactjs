de7120b291960ca89a7396d0f5f826aa23dd4b75
NasirSultan <rainasirsultan123@gmail.com>
2025-01-19 18:27:37 +0500

    u

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import "./App.css";
-
-
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Hotels from './components/Hotels';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-      
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/hotels" element={<Hotels />} />
-        
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
